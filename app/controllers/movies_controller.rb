cf87e2a0815a2c4d8699b6e6a5864f31381a9573
Ubuntu <ubuntu@ip-172-31-88-93.ec2.internal>
2020-09-16 03:33:42 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,39 +11,16 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # @movies = Movie.all
-    
     @all_ratings = Movie.ratings
     
     @sort = params[:sort] || session[:sort]
-    session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'R'=>''}
+    # session[:ratings] = session[:ratings] || {'G'=>'', 'PG'=>'', 'PG-13'=>'', 'R'=>''}
     @t_param =  params[:ratings] || session[:ratings]
     
     session[:sort] = @sort
     session[:ratings] = @t_param
-    
-    # params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
-    # t_param =  params[:rating] || 
+
     @movies = Movie.where(rating: session[:ratings].keys).order(session[:sort])
-    
-    # @all_ratings = Movie.all_ratings
-    # sort_by = params[:sort_by] || session[:sort_by]
-    # session[:sort_by] = sort_by
-    # @table_header = 'hilite' if sort_by == 'title'
-    # @release_date_header = 'hilite' if sort_by == 'release_date'
-    # @all_ratings = Movie.ratings
-    
-    # if params.keys.include? "ratings"
-    #   @ratings = params[:ratings].keys if params[:ratings].is_a? Hash
-    #   @ratings = params[:ratings] if params[:ratings].is_a? Array
-    # elsif session.keys.include? "ratings"
-    #   @ratings = session[:ratings]
-    # else
-    #   @ratings = @all_ratings
-    # end
-    # session[:ratings] = @ratings
-    # redirect_to movies_path(:sort_by => session[:sort_by], :ratings => session[:ratings]) if ! ((params.keys.include? 'sort_by') || (params.keys.include? 'ratings'))
-    # @movies = Movie.where(:rating => @ratings).order(sort_by)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ActiveRecord::Base
-   def self.ratings
-        Movie.select(:rating).distinct.inject([]){|a, m| a.push m.rating}
+    def self.ratings
+        # Movie.select(:rating).distinct.inject([]){|a, m| a.push m.rating}
+        ['G', 'PG', 'PG-13', 'R']
     end
 end
